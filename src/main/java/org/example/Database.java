66f08f927d15c2e946eb64575b3b1d0c44cdf477
liuzhch-Orka <liuzhicheng@hiorka.com>
2024-02-25 10:29:50 +0800

    8.User-specific types

---
+++ src/main/java/org/example/Database.java
@@ -0,0 +1,39 @@
+package org.example;
+
+import javax.inject.Inject;
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Database {
+    private final Map<String, Account> accounts = new HashMap<>();
+
+    @Inject
+    Database() {
+    }
+
+    public Account getAccount(String username) {
+        return accounts.computeIfAbsent(username, Account::new);
+    }
+
+    public static final class Account {
+        private final String username;
+        private BigDecimal balance = BigDecimal.ZERO;
+
+        Account(String username) {
+            this.username = username;
+        }
+
+        public String username() {
+            return username;
+        }
+
+        public BigDecimal balance() {
+            return balance;
+        }
+
+        void deposit(BigDecimal amount) {
+            balance = balance.add(amount);
+        }
+    }
+}
--- src/main/java/org/example/command/LoginCommand.java
+++ src/main/java/org/example/command/LoginCommand.java
@@ -1,21 +1,26 @@
 package org.example.command;
 
+import org.example.Database;
+import org.example.Database.Account;
 import org.example.outputter.Outputter;
 
 import javax.inject.Inject;
 
 public final class LoginCommand extends SingleArgCommand {
+    private final Database database;
     private final Outputter outputter;
 
     @Inject
-    LoginCommand(Outputter outputter) {
+    LoginCommand(Database database, Outputter outputter) {
+        this.database = database;
         this.outputter = outputter;
     }
 
 
     @Override
     public Result handleArg(String username) {
-        outputter.output(username + " is logged in.");
+        Account account = database.getAccount(username);
+        outputter.output(username + " is logged in. Balance: " + account.balance());
         return Result.handled();
     }
 }
